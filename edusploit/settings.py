b2cc9e9a232838b5119371e768eb6d162cb18302
Gbenga Adeyi <adeyigbenga005@gmail.com>
2023-03-01 22:14:28 +0100

    Disable debug flag

--- edusploit/settings.py
+++ edusploit/settings.py
@@ -25,8 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY", default="secret-key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = "RENDER" not in os.environ
-DEBUG = True
+DEBUG = "RENDER" not in os.environ
 ALLOWED_HOSTS = []
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get("RENDER_EXTERNAL_HOSTNAME")
